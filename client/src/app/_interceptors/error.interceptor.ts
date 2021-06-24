563414cffa172526cd8795333eb8565dd16c873b
core.editor <Kyaw.Htay@gmail.com>
2021-06-24 23:06:04 +0100

    Add for UI

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -44,7 +44,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                this.router.navigateByUrl('/server-error',navigationExtras);
               break;
             default:
-              this.toastr.error('SOmething unexpected went wrong.');
+              this.toastr.error('Something unexpected went wrong.');
               console.log(error);
               break;
           }
