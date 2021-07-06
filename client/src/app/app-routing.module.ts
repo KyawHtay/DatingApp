7a588ee40da244e6a793466ec17b7e98522718fe
core.editor <Kyaw.Htay@gmail.com>
2021-07-06 07:43:46 +0100

    add messages for leave page unchanged

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import {HomeComponent} from './home/home.component';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MemberEditComponent} from './members/member-edit/member-edit.component';
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
     {
@@ -31,7 +32,8 @@ const routes: Routes = [
             },
             {
                 path: 'member/edit',
-                component: MemberEditComponent
+                component: MemberEditComponent,
+                canDeactivate:[PreventUnsavedChangesGuard]
             },
             {
                 path: 'lists',
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { take } from 'rxjs/operators';
+import {Component, HostBinding, HostListener, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {ToastrService} from 'ngx-toastr';
+import {take} from 'rxjs/operators';
 import {Member} from 'src/app/_models/members';
 import {User} from 'src/app/_models/user';
 import {AccountService} from 'src/app/_services/account.service';
@@ -8,25 +9,34 @@ import {MembersService} from 'src/app/_services/members.service';
 
 @Component({selector: 'app-member-edit', templateUrl: './member-edit.component.html', styleUrls: ['./member-edit.component.css']})
 export class MemberEditComponent implements OnInit {
+    @ViewChild('editForm') editForm : NgForm;
     member : Member;
     user : User;
+    @HostListener ('window:beforeunload',['$event']) unloadNotification($event:any){
+        if(this.editForm.dirty){
+            $event.returnValue =true;
+        }
 
-    constructor(private accountService : AccountService, private memberService : MembersService,private toastr: ToastrService) {
-      this.accountService.currentUser$.pipe(take(1)).subscribe(user=>this.user=user);
+    }
+
+    constructor(private accountService : AccountService, private memberService : MembersService, private toastr : ToastrService) {
+        this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
     }
 
     ngOnInit(): void {
-      this.loadMember();
+        this.loadMember();
     }
 
-    loadMember(){
-      this.memberService.getMember(this.user.username).subscribe(member=>{
-        this.member = member;
-      })
+    loadMember() {
+        this.memberService.getMember(this.user.username).subscribe(member => {
+            this.member = member;
+        })
     }
-    updateMember(){
-      console.log(this.member);
-      this.toastr.success('Profile update successfully');
+    updateMember() {
+        console.log(this.member);
+        this.toastr.success('Profile update successfully');
+        this.editForm.reset(this.member);
     }
 
 }
+
